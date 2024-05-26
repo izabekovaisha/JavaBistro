a51f814a3f82990174eb80d57a34dce800f01f36
izabekovaisha <izabekovaisha@gmail.com>
2024-05-25 21:51:04 -0400

    Add Order class

--- src/main/java/com/pluralsight/orders/Order.java
+++ src/main/java/com/pluralsight/orders/Order.java
@@ -1,4 +1,86 @@
 package com.pluralsight.orders;
 
+import com.pluralsight.menu.Chips;
+import com.pluralsight.menu.Drink;
+import com.pluralsight.menu.Sandwich;
+
+import java.util.List;
+
 public class Order {
+    private Customer customer;
+    private List<Sandwich> sandwiches;
+    private List<Drink> drinks;
+    private List<Chips> chips;
+
+    public Order(Customer customer, List<Sandwich> sandwiches, List<Drink> drinks, List<Chips> chips) {
+        this.customer = customer;
+        this.sandwiches = sandwiches;
+        this.drinks = drinks;
+        this.chips = chips;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Sandwich> getSandwiches() {
+        return sandwiches;
+    }
+
+    public void setSandwiches(List<Sandwich> sandwiches) {
+        this.sandwiches = sandwiches;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<Drink> drinks) {
+        this.drinks = drinks;
+    }
+
+    public List<Chips> getChips() {
+        return chips;
+    }
+
+    public void setChips(List<Chips> chips) {
+        this.chips = chips;
+    }
+
+    public void addSandwich(Sandwich sandwiches) {
+
+    }
+
+    public void addDrink(Drink drinks) {
+
+    }
+
+    public void addChips(Chips chips) {
+
+    }
+
+    public double calculateDoubleCost() {
+        return 0;
+    }
+
+    public void displayCustomerInfo() {
+
+    }
+
+    public void displayOrderDetails() {
+
+    }
+
+    public void saveReceipt() {
+
+    }
+
+    public void placeOrder() {
+
+    }
+
 }
