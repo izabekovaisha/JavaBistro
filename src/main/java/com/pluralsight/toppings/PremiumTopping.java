d983049322c3b974909f9e03c57de4aad070c6c0
izabekovaisha <izabekovaisha@gmail.com>
2024-05-28 11:57:48 -0400

    Update PremiumTopping class

--- src/main/java/com/pluralsight/toppings/PremiumTopping.java
+++ src/main/java/com/pluralsight/toppings/PremiumTopping.java
@@ -1,12 +1,20 @@
 package com.pluralsight.toppings;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class PremiumTopping implements Topping {
     private String name;
-    private double price;
+    private boolean isMeat;
+    private boolean isCheese;
+    private boolean extraMeat;
+    private boolean extraCheese;
 
-    public PremiumTopping(String name, double price) {
+    private List<String> MEATS = Arrays.asList("steak", "ham", "salami", "roast beef", "chicken", "bacon");
+    private List<String> CHEESES = Arrays.asList("american", "provolone", "cheddar", "swiss");
+
+    public PremiumTopping(String name) {
         this.name = name;
-        this.price = price;
     }
 
     @Override
@@ -14,14 +22,39 @@ public class PremiumTopping implements Topping {
         return name;
     }
 
+    public boolean isMeat() {
+        return isMeat;
+    }
+
+    public boolean isCheese() {
+        return isCheese;
+    }
+
     @Override
     public double getCost(int sandwichSize) {
-        double premiumToppingPrice;
+        double price = 0.0;
         switch (sandwichSize) {
             case 4:
-                premiumToppingPrice = price;
+                price += (isMeat()) ? 1.0 : 0.0; // Add $1.00 for meats
+                price += (extraMeat) ? 0.50 : 0.0; // Add $0.50 for extra meat
+                price += (isCheese()) ? 0.75 : 0.0; // Add $1.00 for meats
+                price += (extraCheese) ? 0.30 : 0.0; // Add $0.50 for extra meat
+                break;
             case 8:
-                premiumToppingPrice = price * 2;
+                price += (isMeat()) ? 2.0 : 0.0; // Add $2.00 for meats
+                price += (extraMeat) ? 1.0 : 0.0; // Add $1.00 for extra meat
+                price += (isCheese()) ? 1.50 : 0.0; // Add $2.00 for meats
+                price += (extraCheese) ? 0.60 : 0.0; // Add $1.00 for extra meat
+                break;
+            case 12:
+                price += (isMeat()) ? 3.0 : 0.0; // Add $3.00 for meats
+                price += (extraMeat) ? 1.50 : 0.0; // Add $1.50 for extra meat
+                price += (isCheese()) ? 2.25 : 0.0; // Add $3.00 for meats
+                price += (extraCheese) ? 0.90 : 0.0; // Add $1.50 for extra meat
+                break;
+            default:
+                break;
         }
+        return price;
     }
 }
--- src/main/java/com/pluralsight/toppings/RegularTopping.java
+++ src/main/java/com/pluralsight/toppings/RegularTopping.java
@@ -1,10 +1,19 @@
 package com.pluralsight.toppings;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class RegularTopping implements Topping {
     private String name;
+    private List<String> REGULAR_TOPPINGS = Arrays.asList("lettuce", "peppers", "onions", "tomatoes", "jalapenos", "cucumbers", "pickles", "guacamole", "mushrooms");
+    private List<String> SAUCES = Arrays.asList("mayo", "mustard", "ketchup", "ranch", "thousand islands", "vinaigrette");
+    List<String> SIDES = Arrays.asList("au jus", "sauce");
 
     public RegularTopping(String name) {
         this.name = name;
+        if (!isValidTopping(name)) {
+            throw new IllegalArgumentException("Invalid regular topping: " + name);
+        }
     }
 
     @Override
@@ -15,5 +24,11 @@ public class RegularTopping implements Topping {
     @Override
     public double getCost(int sandwichSize) {
         return 0.0;
+    }
+
+    private boolean isValidTopping(String name) {
+        return REGULAR_TOPPINGS.contains(name.toLowerCase()) ||
+                SAUCES.contains(name.toLowerCase()) ||
+                SIDES.contains(name.toLowerCase());
     }
 }
